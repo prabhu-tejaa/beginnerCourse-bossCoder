3ff3da81144a87942e1d98c7bd80165713820e0d
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-08-25 16:18:42 +0530

    Area of circle

---
+++ src/areaOfCircle.java
@@ -0,0 +1,9 @@
+public class areaOfCircle {
+    public static void main(String[] args){
+        int radius = 2;
+        int squaredRadius = radius * radius;
+        double pi = 22.0 / 7;
+        double area = pi * squaredRadius;
+        System.out.print("Area of circle is: " + area);
+    }
+}
