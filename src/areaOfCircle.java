e8fde3f20fa16bb9991c363ad06a9b32a47a7ef5
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-08-25 17:49:55 +0530

    taking input from the user - Area of circle

--- src/areaOfCircle.java
+++ src/areaOfCircle.java
@@ -1,9 +1,19 @@
+import java.util.*;
 public class areaOfCircle {
     public static void main(String[] args){
         int radius = 2;
         int squaredRadius = radius * radius;
         double pi = 22.0 / 7;
         double area = pi * squaredRadius;
-        System.out.print("Area of circle is: " + area);
+        System.out.println("Area of circle is: " + area);
+
+        // taking input from the users
+
+        Scanner inputFromUser = new Scanner(System.in);
+        System.out.print("Enter the value to get  the area of the circle: ");
+        int inputRadius = inputFromUser.nextInt();
+        int squaredInputRadius = inputRadius * inputRadius;
+        double totalAreaGivenByTheUser = pi * squaredInputRadius;
+        System.out.print("The area of the circle given by the user is: " + totalAreaGivenByTheUser);
     }
 }
