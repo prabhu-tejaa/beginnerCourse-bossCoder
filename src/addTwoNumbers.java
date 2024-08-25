c24a207a48ea0f54b90eaf25b18f5b958e23e034
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-08-25 11:10:35 +0530

    taking values from user

--- src/addTwoNumbers.java
+++ src/addTwoNumbers.java
@@ -1,8 +1,19 @@
+import java.util.*;
 public class addTwoNumbers {
         public static void main(String[] args) {
             int firstNumber = 3;
             int secondNumber = 7;
             int sum = firstNumber+secondNumber;
             System.out.print("Addition of two number 3+7 = " + sum);
+
+            Scanner inputFromUser = new Scanner(System.in);
+            System.out.print("Enter first number: ");
+
+            int firstInputFromUser = inputFromUser.nextInt();
+            System.out.print("Enter second number: ");
+
+            int secondInputFromUser = inputFromUser.nextInt();
+            int AddingOfTwoInputValues = firstInputFromUser+ secondInputFromUser;
+            System.out.println("The sum of your numbers = " + AddingOfTwoInputValues);
     }
 }
