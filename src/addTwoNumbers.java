15edce9c39cddb3040fb16b238d7521c4542f7e6
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-08-25 10:54:33 +0530

    addTwoNumbers

---
+++ src/addTwoNumbers.java
@@ -0,0 +1,8 @@
+public class addTwoNumbers {
+        public static void main(String[] args) {
+            int firstNumber = 3;
+            int secondNumber = 7;
+            int sum = firstNumber+secondNumber;
+            System.out.print("Addition of twon number 3+7 = " + sum);
+    }
+}
