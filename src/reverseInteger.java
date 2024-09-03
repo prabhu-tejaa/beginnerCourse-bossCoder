c367d6d67da23c0f22fad6535156f0a8a812302a
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 18:43:03 +0530

    308. Reversed integer using for loop

--- src/reverseInteger.java
+++ src/reverseInteger.java
@@ -6,11 +6,21 @@ public class reverseInteger {
             System.out.println("Enter any number to reverse it: ");
             int userInput = input.nextInt();
             int reversedVale = 0;
-            while(userInput!=0){
-                int lastDigit = userInput%10;
+
+            //Approach 1
+//            while(userInput!=0){
+//                int lastDigit = userInput%10;
+//                reversedVale = (reversedVale*10)+lastDigit;
+//                userInput = userInput/10;
+//            }
+
+            //Approach 2
+
+            for(int temp = userInput;temp!=0;temp/=10){
+                int lastDigit = temp%10;
                 reversedVale = (reversedVale*10)+lastDigit;
-                userInput = userInput/10;
             }
+
             System.out.println("Reversed value is: " + reversedVale);
         }
     }
