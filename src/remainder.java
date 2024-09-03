7035a55683adafcdd779ff09e90b58c63e7f4257
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 10:39:51 +0530

    298. remainder

---
+++ src/remainder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class remainder {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("To get the remainder of two given values");
+            System.out.println("Enter the firstValue");
+            int firstValue = input.nextInt();
+            System.out.println("Enter the firstValue");
+            int secondValue = input.nextInt();
+            int remainder = firstValue % secondValue;
+            System.out.println("remainder of given value is: " + remainder);
+        }
+    }
