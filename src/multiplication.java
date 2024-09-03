1fe0d20d116ca9c7d309ace271857d54b14ec1be
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 10:35:10 +0530

    229. multiplication

---
+++ src/multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class multiplication {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("To get the multiplication og two given values");
+        System.out.println("Enter the firstValue");
+        int firstValue = input.nextInt();
+        System.out.println("Enter the firstValue");
+        int secondValue = input.nextInt();
+        int multiplication = firstValue * secondValue;
+        System.out.println("Multiplication of given value is: " + multiplication);
+
+    }
+}
