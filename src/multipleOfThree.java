56844f24462ebbc4d911335314a7d427ecc103c1
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 10:58:41 +0530

    228. multiple of 3

---
+++ src/multipleOfThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class multipleOfThree {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("To get weather a number is multiple of 3");
+            System.out.println("Enter the value");
+            int firstValue = input.nextInt();
+            if(firstValue%3 == 0){
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        }
+    }
