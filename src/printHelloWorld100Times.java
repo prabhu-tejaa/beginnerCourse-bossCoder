ffc0d5d0637e9e80e9a6abfb5a0c03c9976e63f9
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 11:28:15 +0530

    hello world 100 times

---
+++ src/printHelloWorld100Times.java
@@ -0,0 +1,7 @@
+public class printHelloWorld100Times {
+    public static void main(String[] args){
+        for(int i = 0; i<=100; i++){
+            System.out.println(i+" Hello world");
+        }
+    }
+}
