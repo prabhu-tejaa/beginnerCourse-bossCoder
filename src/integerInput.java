b92458b79205f59a8c4031c8180e1c2c2a807a18
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 10:23:21 +0530

    133. input output

---
+++ src/integerInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class integerInput {
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the integer input you want to return: ");
+    int integerValue = input.nextInt();
+        System.out.println("Return value of integer is: " + integerValue);
+    }
+}
