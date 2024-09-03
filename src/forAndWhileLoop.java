21ebb181b0afd4787d7a278a1870337e481607e3
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 12:23:16 +0530

    same exectuion in for & while loop

---
+++ src/forAndWhileLoop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class forAndWhileLoop {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many times you wanna loop??");
+        int loopingValue = input.nextInt();
+        int iteration = 1;
+        for(int i = 1; i<= loopingValue; i++){
+            System.out.println(i + ". Hello Iam looping in for loop");
+        }
+        while(iteration <= loopingValue){
+            System.out.println(iteration + ". Hello Iam looping in while loop");
+            iteration++;
+        }
+    }
+}
