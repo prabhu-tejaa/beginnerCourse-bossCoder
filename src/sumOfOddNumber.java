84ad23109e5106bfc52918d568234c3bd0586a42
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 18:08:58 +0530

    255. Odd Number

---
+++ src/sumOfOddNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class sumOfOddNumber {
+    public static void main(String[] args){
+
+        // Approach 1
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int userInput = input.nextInt();
+        int sum = 0;
+        for(int i = 1; i<2*userInput;i+=2){
+            sum+=i;
+        }
+        System.out.println("The sum of odd numbers of the given value "+ userInput + " is " + sum);
+
+        //Approach 2
+        System.out.println("The sum of odd numbers of the given value "+ userInput + " is " + (userInput*userInput));
+    }
+}
