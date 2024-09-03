cff51f8923cf809f1c427add3e514a0954947f7c
Prabhu Teja Pamula <prabhutejapamula@yahoo.com>
2024-09-03 10:37:44 +0530

    typo

--- src/multiplication.java
+++ src/multiplication.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class multiplication {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("To get the multiplication og two given values");
+        System.out.println("To get the multiplication of two given values");
         System.out.println("Enter the firstValue");
         int firstValue = input.nextInt();
         System.out.println("Enter the firstValue");
