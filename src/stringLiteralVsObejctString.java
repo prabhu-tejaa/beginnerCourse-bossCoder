941976b8671072934faeba4410c60c4a83fb9b25
teja137 <ajetdream@yahoo.com>
2024-09-20 10:37:14 +0530

    stringLiterals and stringObject differences in the memory

---
+++ src/stringLiteralVsObejctString.java
@@ -0,0 +1,26 @@
+public class stringLiteralVsObejctString {
+    public static void main(String[] args){
+        String name = "Prabhu Teja";
+        String myName = "Prabhu Teja";
+
+        //String literals... Now we are checking if the memory allocation for the staring is the same constant
+        if(name == myName){
+            System.out.println("Both name and myname point of you same literals");
+        } else {
+            System.out.println("Both name and mYname point out to different literals");
+        }
+
+
+        //String object... Now we are checking if the string object is point out to same object in memory or a new one.
+
+        String lastName = new String("Pamula");
+        String mYLastName = new String("Pamula");
+
+        if(lastName == mYLastName){
+            System.out.println("Both lastName and myLastName point out to same object");
+        } else {
+            System.out.println("Both lastName and myLastName point out to a different object");
+        }
+
+    }
+}
