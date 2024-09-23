44dcf0cead1a150f0040bb4486c8f3c4022ecfa7
teja137 <ajetdream@yahoo.com>
2024-09-23 22:25:20 +0530

    pattern 06

--- src/patternProblem06.java
+++ src/patternProblem06.java
@@ -2,13 +2,21 @@ import java.util.*;
 public class patternProblem06 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter number to the 5th star pattern.");
+        System.out.println("Please enter number to the 6th star pattern.");
         int userInput = input.nextInt();
-        for(int i = 0; i < userInput ; i++){
-            for(int j = 0; j < userInput - i ; j++){
-                System.out.print("* ");
+        for(int i=0;i<userInput;i++){
+            for(int j=0;j<userInput;j++){
+                if(i==0 || i==userInput-1 || j==0 || j==userInput-1)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print("_");
+                }
             }
             System.out.println();
         }
+
     }
 }
