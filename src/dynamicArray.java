68a6e0a4d0c10deb6298ed7eced562e9795b1721
teja137 <ajetdream@yahoo.com>
2024-09-24 00:34:49 +0530

    31. Build Array from Permutation

---
+++ src/dynamicArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class dynamicArray {
+    public static void main(String[] args) {
+        List<Integer> array  = new ArrayList<>();
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+
+        System.out.println(array);
+    }
+}
