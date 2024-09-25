693ddb7c6994627a0bfb350282c0dceda425427b
teja137 <ajetdream@yahoo.com>
2024-09-26 02:09:45 +0530

    added comments

--- src/iPlusJisEqualToKInAnArray.java
+++ src/iPlusJisEqualToKInAnArray.java
@@ -9,6 +9,8 @@ public class iPlusJisEqualToKInAnArray {
         int[] userArray = {5,2,18,10,7};
         int n = userArray.length;
 
+        //solution: first find the max value in the array and second create attendance array at assigned its values and third loop and find if k-a[i] is there or not in attendance array
+
         int max = userArray[0];
         for(int i = 0; i <= n-1;i++){
             if(userArray[i] > max){
