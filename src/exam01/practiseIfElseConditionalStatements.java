a3fa47f7c5c973717f1778f08c51cf775490bf48
teja137 <ajetdream@yahoo.com>
2024-10-17 09:41:28 +0530

    practise_02 if else conditional statements

---
+++ src/exam01/practiseIfElseConditionalStatements.java
@@ -0,0 +1,68 @@
+package exam01;
+import java.util.*;
+
+public class practiseIfElseConditionalStatements {
+    public static void main(String[] args) {
+        
+        // 01 even odd
+
+        // int num1  = 6;
+        // if(num1%2 == 0){
+        //     System.out.println(true);
+        // } else {
+        //     System.out.println(false);
+        // }
+
+        // 02 multiple of 5 -> n = 15 -> [5,10,15]
+
+        // int userInput = 25;
+        // int[] answer = new int[userInput/5];
+        // for(int i = 5,j=0;i<=userInput;i=i+5){
+        //     answer[j++]=i;
+        // }
+        // System.out.println(Arrays.toString(answer));
+
+        // 03 return count of all postive numbers
+
+        // int[] userInput = {-1,-2,-3,1,2,3,-9,4};
+        // int count = 0;
+        // for(int i = 0; i<userInput.length;i++){
+        //     if(userInput[i]>0){
+        //         count++;
+        //     }
+        // }
+        // System.out.println(count);
+
+        // 04 leap year -> every 400 year is a leap year, every 100 year is not a leap year every 4 years is a leap year
+        // so kinda always remeebr 400, 100, 4
+
+        // int leapYear = 2024;
+        // if(leapYear%400 == 0){
+        //     System.out.println(true);
+        // } else if(leapYear%100 == 0){
+        //     System.out.println(false);
+        // } else if(leapYear%4==0){
+        //     System.out.println(true);
+        // } else {
+        //     System.out.println(false);
+        // }
+
+        // 05 maximum of 2 integers
+
+        // int num1 = 3;
+        // int num2 = 7;
+        // if(num1>num2){
+        //     System.out.println("num1 is greater");
+        // } else {
+        //     System.out.println("num2 is greater");
+        // }
+
+        // 06 check weather a number is multiple of 3 or not
+
+        // int num = 777;
+        // System.out.println(num%3 == 0);
+
+
+    }
+
+}
