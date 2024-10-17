ab4126d4c18b482aba3bb4c250b4708cd2d715b2
teja137 <ajetdream@yahoo.com>
2024-10-17 09:12:48 +0530

    practise_01 basics of programming

---
+++ src/exam01/practiseBasicsOfProgramming.java
@@ -0,0 +1,59 @@
+package exam01;
+
+public class practiseBasicsOfProgramming {
+    public static void main(String[] args) {
+        //01 add two numbers
+
+        // int num1 = 3;
+        // int num2 = 7;
+        // int sum = num1 + num2;
+        // System.out.println(sum);
+
+        //02 return hello world
+
+        // String welcome = "Hello Universe";
+        // System.out.println(welcome);
+
+        // 03 return input
+
+        // int inputNumber = 137;
+        // System.out.println(inputNumber);
+
+        // 04 sum of 3 number
+
+        // int num1 = 01;
+        // int num2 = 03;
+        // int num3 = 07;
+        // int sumOfThreeNums = num1+num2+num3;
+        // System.out.println(sumOfThreeNums);
+
+        // 05 multiply 2 integers
+
+        // int num1 = 3;
+        // int num2 = 7;
+        // int multiplication = num1*num2;
+        // System.out.println(multiplication);
+
+        // 06 subtraction
+
+        // int num1 = 7;
+        // int num2 = 3;
+        // int subtractionOfTwoNumbers = num1-num2;
+        // System.out.println(subtractionOfTwoNumbers);
+
+        // 07 remainder
+
+        // int num1 = 3;
+        // int num2 = 7;
+        // int remainderOfTwoNumbers = num1%num2;
+        // System.out.println(remainderOfTwoNumbers);
+
+        // 08 average of two numbers
+
+        // int num1 = 3;
+        // int num2 = 7;
+        // int averageOfTwoNumbers = (num1+num2)/2;
+        // System.out.println(averageOfTwoNumbers);
+    }
+
+}
